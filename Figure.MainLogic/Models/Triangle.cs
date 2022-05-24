055026a456baa5697fe28850563b86db2ec5154a
MaksimShkodin@outlook.com <MaksimShkodin@outlook.com>
2022-05-24 22:30:50 +0400

    Add project files.

---
+++ Figure.MainLogic/Models/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure.MainLogic.Models
+{
+    public class Triangle : Figure
+    {
+        public double FirstSide { get; set; }
+
+        public double SecondSide { get; set; }
+
+        public double TrirdSide { get; set; }
+
+        public override double GetFigureSquare()
+        {
+            var p = (FirstSide + SecondSide + TrirdSide) / 3;
+            return Math.Sqrt(p * (p - TrirdSide) * (p - SecondSide) * (p - FirstSide));
+        }
+
+        public bool IsRightTriangle()
+        {
+            if (FirstSide * FirstSide + SecondSide * SecondSide == TrirdSide * TrirdSide ||
+                FirstSide * FirstSide + TrirdSide * TrirdSide == SecondSide * SecondSide ||
+                SecondSide * SecondSide + TrirdSide * TrirdSide == FirstSide * FirstSide)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
